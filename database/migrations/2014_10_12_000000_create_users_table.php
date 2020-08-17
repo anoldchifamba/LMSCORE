552fbc07a0e58ca993b6d83badeece4ae4dfb97e
anold chifamba <anoldchifamba@gmail.com>
2020-08-17 09:03:40 +0200

    done category  interface and text editor

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,10 +17,19 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('gender')->nullable()->default('male ');
+            $table->string('date_of_birth')->nullable();
+            $table->tinyInteger('is_subscribed')->nullable()->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('view_count')->default(0);
+            $table->integer('role_id')->default(4);
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
+
         });
     }
 
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->middleware('verified');
+
+Route::resource('categories', 'CategoryController');
+
+Route::resource('courses', 'CourseController');
+
+Route::resource('comments', 'CommentController');
+
+Route::resource('courseUsers', 'CourseUserController');
+
+Route::resource('items', 'ItemController');
+
+Route::resource('payments', 'PaymentController');
+
+Route::resource('users', 'UserController');
+
+Route::resource('views', 'ViewsController');
+
+Route::resource('roles', 'RoleController');
