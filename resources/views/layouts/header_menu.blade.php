dded6ed98e39e049a6cb2e29cfec7dbdea029c6f
anold chifamba <anoldchifamba@gmail.com>
2020-09-09 15:26:54 +0200

    done intergrating material ,playlist and comments page3

--- resources/views/layouts/header_menu.blade.php
+++ resources/views/layouts/header_menu.blade.php
@@ -6,20 +6,43 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12 col-lg-6 d-none d-md-flex flex-wrap justify-content-center justify-content-lg-start mb-3 mb-lg-0">
-                        <div class="header-bar-email d-flex align-items-center">
-                            <i class="fa fa-envelope"></i><a href="mailto:admissions@atacademy.co.zw">admissions@atacademy.co.zw</a>
-                        </div><!-- .header-bar-email -->
+
+                        <h6 class="site-title"><a href="{{ url('index') }}" rel="home"><img src="{{ asset('log/logo.png') }}" height="50px" alt=""></a></h6>
 
                         <div class="header-bar-text lg-flex align-items-center">
-                            <p><i class="fa fa-phone"></i>0242779156 </p>
+
                         </div><!-- .header-bar-text -->
                     </div><!-- .col -->
 
                     <div class="col-12 col-lg-6 d-flex flex-wrap justify-content-center justify-content-lg-end align-items-center">
                         <div class="header-bar-search">
                             <form class="flex align-items-stretch">
-                                <input type="search" placeholder="What would you like to learn?">
-                                <button type="submit" value="" class="flex justify-content-center align-items-center"><i class="fa fa-search"></i></button>
+                                <div class="dropdown">
+                                    <button style="background-color: white;" class="btn btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Filter by : Courses
+                                    </button>
+                                    <div  class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        @foreach($courses as $course)
+                                            {{--                                            @if($category->id ==$course->category_id)--}}
+                                            <a class="dropdown-item" href="/courses/{{ $course->id }}
+                                                ">{{ $course->title }}</a>
+                                            {{--                                            @endif--}}
+                                        @endforeach
+                                    </div>
+                                </div>
+                                <div class="dropdown" >
+                                    <button style="background-color: white;" class="btn btn-sm dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Category
+                                    </button>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        @foreach($categories as $category)
+                                            {{--                                            @if($category->id ==$course->category_id)--}}
+                                            <a class="dropdown-item" href="/courses/{{ $category->id }}
+                                                ">{{ $category->name }}</a>
+                                            {{--                                            @endif--}}
+                                        @endforeach
+                                    </div>
+                                </div>
                             </form>
                         </div><!-- .header-bar-search -->
 
@@ -28,16 +51,16 @@
                             <ul class="flex justify-content-center align-items-center py-2 pt-md-0">
                                 @if (Route::has('login'))
 
-                                        @auth
-                                            <a href="{{ url('/home') }}">Home</a>
-                                        @else
+                                    @auth
+                                        <a href="{{ url('/home') }}">Home</a>
+                                    @else
 
-                                <li><a href="{{ route('login') }}">Login</a></li>
-                                            @if (Route::has('register'))
-                                                <li> <a href="{{ route('register') }}">Register</a></li>
+                                        <li><a href="{{ route('login') }}">Login</a></li>
+                                        @if (Route::has('register'))
+                                            <li> <a href="{{ route('register') }}">Register</a></li>
+                                        @endif
+                                    @endauth
                                 @endif
-                                @endauth
-                                    @endif
                             </ul>
                         </div><!-- .header-bar-menu -->
                     </div><!-- .col -->
@@ -45,26 +68,37 @@
             </div><!-- .container-fluid -->
         </div><!-- .top-header-bar -->
 
-        <div class="nav-bar">
+        <div class="nav-bar" >
             <div class="container">
                 <div class="row">
-                    <div class="col-9 col-lg-3">
+                    <div class="col-9 col-lg-2">
                         <div class="site-branding">
-                            <h1 class="site-title"><a href="{{ url('index') }}" rel="home"><img src="{{ asset('log/logo.png') }}" alt=""></a></h1>
+                            {{--                            <h6 class="site-title"><a href="{{ url('index') }}" rel="home"><img src="{{ asset('log/logo.png') }}" height="50px" alt=""></a></h6>--}}
                         </div><!-- .site-branding -->
                     </div><!-- .col -->
 
-                    <div class="col-3 col-lg-9 flex justify-content-end align-content-center">
+                    <div class="col-3 col-lg-9 flex justify-content-end align-content-center" >
                         <nav class="site-navigation flex justify-content-end align-items-center">
-                            <ul class="flex flex-column flex-lg-row justify-content-lg-end align-content-center">
-                                <li class="current-menu-item"><a href="{{ url('index') }}">Home</a></li>
 
-                                <li><a href="{{ url('about') }}">About</a></li>
-                                <li><a href="{{ url('course') }}">Courses</a></li>
-                                <li><a href="{{ url('blog') }}">blog</a></li>
-                                <li><a href="{{ url('contact') }}">Contact</a></li>
+                            @foreach($categories as $category)
+                                {{--                                <li><a href="/categories/{{ $category->id }}"></a></li>--}}
+                                {{--                                   <a href="/categories/{{ $category->id }}">{{$category->name}}</a>--}}
+                                <div class="dropdown">
+                                    <button style="background-color:transparent; color: white;" class="btn  " type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        {{$category->name}}
+                                    </button>
+                                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        @foreach($courses as $course)
+                                            @if($category->id ==$course->category_id)
+                                                <a class="dropdown-item" href="/courses/{{ $course->id }}
+                                                    ">{{ $course->title }}</a>
+                                            @endif
+                                        @endforeach
+                                    </div>
+                                </div>
+                            @endforeach
 
-                            </ul>
+
 
                             <div class="hamburger-menu d-lg-none">
                                 <span></span>
